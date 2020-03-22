61a00fad993a08c10fe600caaf2acdd9d0801c26
Chandra <chandrakiranapoetracendana@gmail.com>
2020-03-22 22:16:40 +0700

    Chandra KP

---
+++ src/1174079/3/bacadataset.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 19:42:42 2020
+
+@author: Chandra
+"""
+
+import pandas as pd
+dataset = pd.read_csv('F:/Poltekpos/D4 TI 3C/Semester 6/Kecerdasan Buatan/Github/Upload 18 Maret 2020 github tugas 3/src/1174079/3/data.csv', sep=',')
+dataset.head()
