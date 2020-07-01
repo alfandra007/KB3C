3412672e4094a32de0a925e50d16cd91b93e17f5
unknown <sekar>
2020-07-01 09:24:00 +0700

    1174075

---
+++ src/1174075/8/praktek.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 16:36:09 2020
+
+@author: Sekar
+"""
+
+import scipy.io as io
+import matplotlib.pyplot as plt
+import scipy.ndimage as nd
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D 
+
+voxels = io.loadmat("/content/3DShapeNets/volumetric_data/flower_pot/30/test/flower_pot_000000010_1.mat")['instance']
+voxels = np.pad(voxels, (1, 1), 'constant', constant_values=(0, 0))
+voxels = nd.zoom(voxels, (2, 2, 2), mode='constant', order=0)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.voxels(voxels, edgecolor="chartreuse")
+
+plt.show()
+plt.savefig('flower_pot')
