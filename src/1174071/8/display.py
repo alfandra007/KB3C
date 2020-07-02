9100de06e09826fb2b3677c66aa308b81d179cbb
jayker1 <muhamad.gani33@gmail.com>
2020-07-02 20:59:19 +0700

    1174071

---
+++ src/1174071/8/display.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 14:56:53 2020
+
+@author: muham
+"""
+
+# In[]
+            
+import scipy.io as io
+voxels = io.loadmat("data/3DShapeNets/volumetric_data/chair/30/test/chair_000000000_1.mat")['instance']
+#%%
+import numpy as np
+voxels = np.pad(voxels, (1, 1), 'constant', constant_values=(0, 0))
+#%%
+import scipy.ndimage as nd
+voxels = nd.zoom(voxels, (2, 2, 2), mode='constant', order=0)
+#%%
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D 
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.voxels(voxels, edgecolor="red")
+
+plt.show()
+plt.savefig('data')
